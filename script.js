de185291235e07618255ae82278906e9a6f88390
Andrey Lazarew <koman4bk@bk.ru>
2022-02-05 14:00:54 +0300

    ДЗ к уроку № 6

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ let countGuess = 10,
 
 function guessNumber (num) {
   let userNumber = prompt('Угадай число от 1 до 100');
-  countGuess -= 1;
    
   if (userNumber === null) {
     alert('Игра окончена!!');
     return;
   } else if (userNumber > num && userNumber !== '' && countGuess > 0) {
+    countGuess -= 1;
     alert('Загаданное число меньше' + ', ' + 'осталось попыток' + ' ' + countGuess);
+    
+    // guessNumber(num);
   } else if (userNumber < num && userNumber !== '' && countGuess > 0) {
+    countGuess -= 1;
     alert('Загаданное число больше' + ', ' + 'осталось попыток' + ' ' + countGuess);
+    
+    // guessNumber(num);
   } else if (userNumber == num) {
     confirm('Поздравляю, Вы угадали' + ' ' + 'за' + ' ' + (counter - countGuess) + ' ' + 'попыток' + ' ' + 'Хотите сыграть еще?');
+    
+    return; 
+    
   } else if (countGuess === 0) {
     confirm('Вы использовали все попытки! Хотите сыграть еще?)');
     return;
-  }else {
+        
+  } else {
     alert('Введи число');
+    guessNumber(num);
   }
+    
   guessNumber(num);
 }
   
